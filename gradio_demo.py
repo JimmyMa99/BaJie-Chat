a47c72cfa19684ec9e192ed934e5d053d0f03972
MrCat <a976466014@gmail.com>
2024-07-25 01:32:18 +0800

    gradio

--- gradio_demo.py
+++ gradio_demo.py
@@ -6,4 +6,5 @@ base_path = './JimmyMa99/BaJie-Chat-llama3_1-8b'
 from modelscope import snapshot_download
 model_dir = snapshot_download('JimmyMa99/BaJie-Chat-llama3_1-8b',cache_dir='./')
 os.system(f'cd .. && pip install lmdeploy')
+os.system(f'cd .. && pip install gradio==4.13.0')
 os.system(f'lmdeploy serve gradio {base_path} --model-name llama3 --server-port 7860')
